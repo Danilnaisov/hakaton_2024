fbf2c3a8e4b2454bc060d1b2f63d9ea6d456baed
Danil <117021619+Danilnaisov@users.noreply.github.com>
2024-04-17 00:24:40 +0500

    update start bat files

--- startPy.bat
+++ startPy.bat
@@ -1,2 +1,2 @@
 REM Запуск телеграм-бота
-start "C:\Users\danil\AppData\Local\Programs\Python\Python312\python.exe"  "C:\Users\danil\Documents\GitHub\hakaton_2024\TelegramNewsBot\tgBot.py"
+start "C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python312\python.exe"  "C:\Users\%USERNAME%\Documents\GitHub\hakaton_2024\TelegramNewsBot\tgBot.py"
--- startWeb.bat
+++ startWeb.bat
@@ -1,2 +1,2 @@
 REM Запуск веб-приложения на ReactJS
-start "" /D "C:\Users\danil\Documents\GitHub\hakaton_2024\hakaton-site" npm start
+start "" /D "C:\Users\%USERNAME%\Documents\GitHub\hakaton_2024\hakaton-site" npm start
